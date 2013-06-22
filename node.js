5fc4eb5ca326811f1aa8c9279aa912eaf2a42060
serhiy <ryba02@bigmir.net>
2013-06-22 22:30:22 +0300

    Update node.js for task 2

--- node.js
+++ node.js
@@ -1,8 +1,33 @@
 #!/usr/bin node
 
-var fs = require('fs');
-var outfile = "hello.txt"
+function get_100_primes() {
+	
+	var prime_numbers = [3]
+	var test_number = 3
 
-var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);  
+	while  (prime_numbers.length < 100) {
+		test_number = test_number + 2
+
+		var primary = true
+
+		for(var i in prime_numbers) {
+			if (test_number % prime_numbers[i] == 0) {
+				primary = false
+				break
+			}
+		}
+
+		if (primary)
+			prime_numbers.push(test_number)
+
+	}
+
+	prime_numbers.slice(0, 0, 2)
+
+	return prime_numbers
+}
+
+primer_string = get_100_primes()
+
+fs.writeFileSync(outfile, primer_string);  
 console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
